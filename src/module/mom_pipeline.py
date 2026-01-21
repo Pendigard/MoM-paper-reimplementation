337e0e888fb77fd16e654a81dbb6d464a71f6d05
Célian <celvas@me.com>
2026-01-21 12:08:19 +0100

    Backward linear attention varlen

--- src/module/mom_pipeline.py
+++ src/module/mom_pipeline.py
@@ -1,22 +1,22 @@
 import torch
 import torch.nn as nn
-from src.module.mom_varlen import MoM
+from src.module.mom_varlen import MoM, LinearAttentionVarlenModule
 
 
 
 
 class MoMPipeline(nn.Module):
-    def __init__(self, input_dim: int, hidden_dim: int, output_dim: int, embedding_dim: int, num_memories: int, k: int, output_activation: nn.Module = nn.Identity(), *args, **kwargs):
+    def __init__(self, input_dim: int, hidden_dim: int, output_dim: int, embedding_dim: int, num_memories: int, k: int, output_activation: nn.Module = nn.Identity(), mom_implementation : nn.Module = MoM, update_module : nn.Module = LinearAttentionVarlenModule(use_triton = True), *args, **kwargs):
         super().__init__()
         self.embedding = nn.Embedding(num_embeddings=input_dim, embedding_dim=embedding_dim)
-        self.mom = MoM(input_dim=embedding_dim, hidden_dim=hidden_dim, num_memories=num_memories, k=k)
+        self.mom = mom_implementation(input_dim=embedding_dim, hidden_dim=hidden_dim, num_memories=num_memories, k=k, update_module=update_module)
         self.output_layer = nn.Linear(hidden_dim, output_dim)
         self.output_activation = output_activation
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         x_emb = self.embedding(x)
-        M_0 = torch.zeros(x.shape[1], self.mom.num_memories + 1, self.mom.hidden_dim, self.mom.hidden_dim, device=x.device)
-        mom_out, _ = self.mom(x_emb, M_0)
+        M_0 = torch.zeros(self.mom.hidden_dim, self.mom.hidden_dim, device=x.device)
+        mom_out = self.mom(x_emb, M_0)
         out = self.output_layer(mom_out)
         out = self.output_activation(out)
         return out
--- src/module/mom_varlen.py
+++ src/module/mom_varlen.py
@@ -6,7 +6,7 @@ import triton
 import triton.language as tl
 
 @triton.jit
-def linear_attn_varlen_kernel(q_ptr, k_ptr, v_ptr, o_ptr, # Sans offset
+def linear_attn_varlen_kernel(q_ptr, k_ptr, v_ptr, o_ptr, m_ptr, # Sans offset
                 s_ptr, M_0_ptr, # Avec offset
                 d : tl.constexpr, # hidden_dim de la mémoire
                 BN: tl.constexpr, # Taille du bloc de mémoire à traiter
@@ -26,15 +26,18 @@ def linear_attn_varlen_kernel(q_ptr, k_ptr, v_ptr, o_ptr, # Sans offset
     pid_j = tl.program_id(1) # Id j : quel bout de M_p on traite
     j = pid_j * BN + tl.arange(0, BN) # Début du bloc de M (BN,)
     j_mask = j < d # Masque pour ne pas dépasser la dimension de M
-    start = tl.load(s_ptr + pid_p).to(tl.int32)      # Début de la séquence p
-    end = tl.load(s_ptr + pid_p + 1).to(tl.int32)    # Fin de la séquence p
+    start = tl.load(s_ptr + pid_p).to(tl.int32) # Début de la séquence p
+    end = tl.load(s_ptr + pid_p + 1).to(tl.int32) # Fin de la séquence p
     # M_cols = tl.zeros((d, BN), dtype=tl.float32)
     i = tl.arange(0, d) # (d,)
 
     # [None, :] : ajoute une dimension à l'indice 0 (unsqueeze(0))
     # [:, None] : ajoute une dimension à l'indice 1 (unsqueeze(1))
-    offs = i[:, None] * d + j[None, :]          # (d, BN)
-    mask = j_mask[None, :]  # (1, BN)
+    offs = i[:, None] * d + j[None, :] # (d, BN)
+
+    # Masque pour la sauvegarde m à chaque étape, on s'assure que les colonnes ne dépassent pas d
+    # Pas besoin de masque pour les lignes car i < d toujours
+    mask = j_mask[None, :] # (1, BN)       
     M_cols = tl.load(M_0_ptr + offs, mask=mask, other=0.0).to(tl.float32)  # (d, BN)
     
     t = start
@@ -44,45 +47,118 @@ def linear_attn_varlen_kernel(q_ptr, k_ptr, v_ptr, o_ptr, # Sans offset
         # M += kt @ vt
         # Produit externe partiel
         M_cols = M_cols + kt[:, None] * vt[None, :]  # (d, BN)
+        tl.store(m_ptr + t * d * d + offs, M_cols.to(tl.float32), mask=mask)
         # Calcul de o_t = q_t @ M
         q_t = tl.load(q_ptr + t * d + i, mask=True, other=0.0).to(tl.float32) # (d,)
         o_cols = tl.sum(M_cols * q_t[:, None], axis=0)  # (BN,)
         tl.store(o_ptr + t * d + j, o_cols.to(tl.float32), mask=j_mask)
         t += 1
 
+@triton.jit
+def linear_attn_varlen_backward_kernel(q_ptr, k_ptr, v_ptr, m_ptr, grad_o_ptr,
+                s_ptr,
+                grad_q_ptr, grad_k_ptr, grad_v_ptr,
+                d : tl.constexpr,
+                BN: tl.constexpr,
+                ):
+    pid_p = tl.program_id(0)
+    pid_j = tl.program_id(1)
+    j = pid_j * BN + tl.arange(0, BN)
+    j_mask = j < d
+    start = tl.load(s_ptr + pid_p).to(tl.int32)
+    end = tl.load(s_ptr + pid_p + 1).to(tl.int32)
+    i = tl.arange(0, d)
+    offs = i[:, None] * d + j[None, :]
+    mask = j_mask[None, :]
+    t = end - 1
+    q_T_delta_acc = tl.zeros((d, BN), dtype=tl.float32)
+    delta_T_q_acc = tl.zeros((BN, d), dtype=tl.float32)
+    while t >= start:
+        kt = tl.load(k_ptr + t * d + i, mask=True, other=0.0).to(tl.float32) # (d,)
+        vt = tl.load(v_ptr + t * d + j, mask=j_mask, other=0.0).to(tl.float32) # (BN,)
+        qt = tl.load(q_ptr + t * d + i, mask=True, other=0.0).to(tl.float32) # (d,)
+        m = tl.load(m_ptr + t * d * d + offs, mask=mask, other=0.0).to(tl.float32) # (d, BN)
+        delta_o = tl.load(grad_o_ptr + t * d + j, mask=j_mask, other=0.0).to(tl.float32) # (BN,)
 
+        grad_v_t = tl.sum(kt[None, :] * delta_T_q_acc, axis=1)  # (BN,)
+        grad_k_t = tl.sum(vt[None, :] * q_T_delta_acc, axis=1)  # (d,)
+        grad_q_t = tl.sum(delta_o[None, :] * m, axis=1)  # (d,)
+
+        q_T_delta_acc += qt[:, None] * delta_o[None, :] # (d, BN)
+        delta_T_q_acc += delta_o[:, None] * qt[None, :] # (BN, d)
+        
+        tl.store(grad_q_ptr + t * d + i, grad_q_t.to(tl.float32), mask=True)
+        tl.store(grad_k_ptr + t * d + i, grad_k_t.to(tl.float32), mask=True)
+        tl.store(grad_v_ptr + t * d + j, grad_v_t.to(tl.float32), mask=j_mask)
+
+        t -= 1
+
+class LinearAttentionVarlenTriton(torch.autograd.Function):
+    @staticmethod
+    def forward(ctx, q, k, v, s, M0):
+        """
+        @brief Wrapper pour le kernel triton de la linear attention avec varlen
+        @param q : Query de taille (T_total, d)
+        @param k : Key de taille (T_total, d)
+        @param v : Value de taille (T_total, d)
+        @param s : Séquence d'indices de taille (num_seqs + 1,)
+        @param M0 : Mémoire initiale de taille (d, d)
+        @return : Sortie de taille (T_total, d)
+        """
+        # q,k,v: (Tt, d) CUDA contiguous
+        q = q.contiguous()
+        k = k.contiguous()
+        v = v.contiguous()
+        s = s.contiguous()
+        M0 = M0.contiguous()
+        Tt, d = q.shape
+        P = s.numel() - 1
+        BN = 32
+
+        o = torch.empty_like(q)
+        m = torch.empty((Tt, d, d), device=q.device, dtype=q.dtype)
+
+        grid = (P, triton.cdiv(d, BN))
+        linear_attn_varlen_kernel[grid](
+            q, k, v, o, m,
+            s, M0,
+            d=d,
+            BN=BN,
+            num_warps=4
+        )
+
+        ctx.s = s
+        # ctx.M0 = M0
+        ctx.save_for_backward(q, k, v, m)
+        return o
+
+    @staticmethod
+    def backward(ctx, grad_o):
+        q, k, v, m = ctx.saved_tensors
+        s = ctx.s
+        _, d = q.shape
+        P = s.numel() - 1
+        BN = 32
+
+        grad_q = torch.empty_like(q)
+        grad_k = torch.empty_like(k)
+        grad_v = torch.empty_like(v)
+
+        grid = (P, triton.cdiv(d, BN))
+        linear_attn_varlen_backward_kernel[grid](
+            q, k, v, m, grad_o,
+            s,
+            grad_q, grad_k, grad_v,
+            d=d,
+            BN=BN,
+            num_warps=4
+        )
+        # On retourne les gradients dans l'ordre des entrées de la fonction forward
+        # None pour s et M0 car se ne sont pas des paramètres apprenables
+        return grad_q, grad_k, grad_v, None, None 
 
 def linear_attn_varlen_triton(q, k, v, s, M0):
-    """
-    @brief Wrapper pour le kernel triton de la linear attention avec varlen
-    @param q : Query de taille (T_total, d)
-    @param k : Key de taille (T_total, d)
-    @param v : Value de taille (T_total, d)
-    @param s : Séquence d'indices de taille (num_seqs + 1,)
-    @param M0 : Mémoire initiale de taille (d, d)
-    @return : Sortie de taille (T_total, d)
-    """
-    # q,k,v: (Tt, d) CUDA contiguous
-    q = q.contiguous()
-    k = k.contiguous()
-    v = v.contiguous()
-    s = s.contiguous()
-    M0 = M0.contiguous()
-    _, d = q.shape
-    P = s.numel() - 1
-    BN = 32
-
-    o = torch.empty_like(q)
-
-    grid = (P, triton.cdiv(d, BN))
-    linear_attn_varlen_kernel[grid](
-        q, k, v, o,
-        s, M0,
-        d=d,
-        BN=BN,
-        num_warps=4
-    )
-    return o
+    return LinearAttentionVarlenTriton.apply(q, k, v, s, M0)
 
 def linear_attn_varlen(q, k, v, s, M0):
     """
@@ -107,7 +183,7 @@ def linear_attn_varlen(q, k, v, s, M0):
             kt = k[t].unsqueeze(-1)  # (d, 1)
             vt = v[t].unsqueeze(0)   # (1, d)
             M = M + (kt @ vt)          # (d, d)
-            o[t] = q[t] @ M
+            o[t] = q[t] @ M  # (d,)
 
     return o
 
@@ -122,18 +198,14 @@ def first_idx(tensor: torch.Tensor) -> torch.Tensor:
     first_idx = torch.nonzero(is_start, as_tuple=True)[0]
     return first_idx
 
-class LinearAttentionVarlen(nn.Module):
-    def __init__(self):
+class LinearAttentionVarlenModule(nn.Module):
+    def __init__(self, use_triton: bool = False):
         super().__init__()
-        self.update_function = linear_attn_varlen
-
-    def forward(self, q : torch.Tensor, k : torch.Tensor, v : torch.Tensor, s : torch.Tensor, M_0 : torch.Tensor) -> torch.Tensor:
-        return self.update_function(q, k, v, s, M_0)
-
-class LinearAttentionVarlenTriton(nn.Module):
-    def __init__(self):
-        super().__init__()
-        self.update_function = linear_attn_varlen_triton
+        self.use_triton = use_triton
+        if self.use_triton:
+            self.update_function = linear_attn_varlen_triton
+        else:
+            self.update_function = linear_attn_varlen
 
     def forward(self, q : torch.Tensor, k : torch.Tensor, v : torch.Tensor, s : torch.Tensor, M_0 : torch.Tensor) -> torch.Tensor:
         return self.update_function(q, k, v, s, M_0)
@@ -160,10 +232,9 @@ class MoM(nn.Module):
         self.W_g = nn.Linear(input_dim, num_memories)  # routing over locals only
         self.W_q = nn.Linear(input_dim, hidden_dim)
 
-        self.update_module = update_module or LinearAttentionVarlen()
+        self.update_module = update_module or LinearAttentionVarlenModule()
         self.softmax = nn.Softmax(dim=-1)
 
-    @torch.no_grad()
     def build_varlen_pack(self, X: torch.Tensor, indices: torch.Tensor, scores: torch.Tensor):
         """
         @brief Réorganise le batch X pour le kernel varlen
@@ -279,3 +350,28 @@ class MoM(nn.Module):
             accumulate=True
         )
         return o
+
+if __name__ == "__main__":
+    torch.autograd.set_detect_anomaly(True)  # utile pour localiser NaN/inplace/etc
+    T = 10
+    B = 2
+    Din = 16
+    d = 8
+    M = 4
+    k = 2
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+    mom = MoM(input_dim=Din, hidden_dim=d, num_memories=M, k=k, update_module=LinearAttentionVarlenModule(use_triton=True)).train()
+    mom = mom.to(device)
+    X  = torch.randn(T, B, Din, requires_grad=True).to(device)
+    X.retain_grad()
+    M0 = torch.randn(d, d, requires_grad=True).to(device)  # si M0 doit recevoir des grads (sinon requires_grad=False)
+
+    o = mom(X, M0)                    # (T,B,d)
+    loss = o.pow(2).mean()            # scalaire
+    loss.backward()
+
+    print("X.grad None?", X.grad is None, "mean|grad|", None if X.grad is None else X.grad.abs().mean().item())
+    for name, p in mom.named_parameters():
+        if p.requires_grad:
+            print(name, "grad None?", p.grad is None, "mean|grad|", None if p.grad is None else p.grad.abs().mean().item())
